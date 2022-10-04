4a7d710bfe7663cfa541b223020316004c3a48ca
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2022-10-04 14:14:37 -0700

    1

---
+++ HelloWorld.java
@@ -0,0 +1,33 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run() {
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        } else{
+            System.out.print("FAILURE");
+        }
+
+        if (b == 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if(c != 1)
+
+    }
+}
+}
+
+public static void main(String[] args)
+{        
+HelloWorld hw = new HelloWorld();
+hw.run();
+}
+}
