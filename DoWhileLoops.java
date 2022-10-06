d7240ba42af69155fa9aad4794664fd1f2a6982d
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2022-10-06 14:07:46 -0700

    just finished do while loops

--- DoWhileLoops.java
+++ DoWhileLoops.java
@@ -1,33 +1,39 @@
-
-/**
- * Write a description of class DoWhileLoops here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class DoWhileLoops
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class DoWhileLoops
-     */
-    public DoWhileLoops()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        int i = 0;
+        do  {
+            System.out.println("i is " + i + " but is less than 15");
+            i += 1;
+        } while(i < 15);
+        
+        
+        int k = 10;
+        do { 
+            System.out.println("k is " + k + " but is greater than 5");
+            k -= 1;
+        } while (k < 5);
+    
+        
+        int a = 1;
+        int b = 10;
+        do {
+            System.out.println("a is " + a + " and b is " + b + " and that is cool");
+            a += 1;
+            b += 1;
+        }while(a < 10 && b <= 20);
+        }
+        
+      
+    public static void main(String[] args)
+    {        
+        DoWhileLoops hw = new DoWhileLoops();
+        hw.run();
     }
 }
