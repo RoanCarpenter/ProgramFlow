ffc0efae1f19ec792f9234236f4c364dfaaa503e
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2022-10-06 14:23:55 -0700

    middle of forloops

---
+++ ForLoops.java
@@ -0,0 +1,16 @@
+public class ForLoops
+{
+
+    public void run() {
+        for (int i = 0; i >= 15; i += 1) {
+            System.out.println(+ i + "is less than or equal to 15");
+        }
+
+    }
+
+    public static void main(String[] args)
+    {        
+        ForLoops hw = new ForLoops();
+        hw.run();
+    }
+}
